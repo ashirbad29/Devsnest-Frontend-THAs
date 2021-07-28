2744631d3cbe19961d8163dbf9c14d6ccea4d029
ashirbad <ashirbadbehera29@gmail.com>
2021-07-28 17:59:51 +0530

    💄 day 25 complete and tailwind is lob

---
+++ day-25/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App flex flex-col justify-center items-center h-screen'>
+			<section className='container h-2/5 w-1/2 rounded-lg p-5 shadow-md bg-white'>
+				<h1 className='font-bold text-green-400 text-2xl text-left'>Hello</h1>
+				<p className='text-left  text-gray-400 text-lg'>
+					i am a Pro Koder, this is a custom Card made using tail wind css
+				</p>
+			</section>
+			<p className='mt-9  text-blue-300'>made with 💜 by @ashirbad</p>
+		</div>
+	);
+}
+
+export default App;
